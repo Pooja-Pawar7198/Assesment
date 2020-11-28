e310045282ed401debab85ef8c1e91f1c7542cd2
Pooja Sadashiv Pawar <72441903+Pooja-Pawar7198@users.noreply.github.com>
2020-11-28 19:40:10 +0530

    Update NumToWord.java

--- NumberToWord/src/NumToWord.java
+++ NumberToWord/src/NumToWord.java
@@ -3,13 +3,17 @@ import java.util.Scanner;
 public class NumToWord 
 {
 	//String array for word representation of 1 to 19 integer values
-	 static String onePlace[] = { "", "one ", "two ", "three ", "four ", "five ", "six ", "seven ", "eight ", 
-             "nine ", "ten ", "eleven ", "twelve ", "thirteen ", "fourteen ", "fifteen ",  "sixteen ",
-             "seventeen ", "eighteen ",  "nineteen " }; 
+	 static String onePlace[] = { "", "One ", "Two ", "Three ", "Four ", 
+                 "Five ", "Six ", "Seven ", "Eight ", 
+                 "Nine ", "Ten ", "Eleven ", "Twelve ", 
+                 "Thirteen ", "Fourteen ", "Fifteen ", 
+                 "Sixteen ", "Seventeen ", "Eighteen ", 
+                 "Nineteen " }; 
 	 
 	//String array for word representation of 20,30,40,50,60,70,80,90  integer values
-	  static String tensPlace[] = { "", "", "twenty ", "thirty ", "forty ", 
-                                   "fifty ", "sixty ", "seventy ", "eighty ", "ninety " }; 
+	  static String tensPlace[] = { "", "", "Twenty ", "Thirty ", "Forty ", 
+                                    "Fifty ", "Sixty ", "Seventy ", "Eighty ", 
+                                    "Ninety " }; 
 	  
 	// function for n is 1 or 2-digit number 
 	  static String numToWords(int n)
@@ -53,20 +57,39 @@ public class NumToWord
 				 out= numToWords(n/100000) + "Lakh " + convertToWords(n%100000);
 			  }
 		  
-		// handles digits at ten millions and hundred  millions places ( for Crore)
-		  else if(n>=10000000 && n<1000000000) {
-				 out=numToWords(n/10000000) + "Crore " + convertToWords(n%10000000);
-			  }
 		 
 		  return out;
 	  }
 	  public static void main(String[] args) 
 	  {
 		  //Accepting input from user
-		  Scanner sc= new Scanner(System.in);
-		  System.out.println("Enter number for conversion");
-		  int n=sc.nextInt();
+            Scanner sc= new Scanner(System.in);
+            System.out.println("Enter number for conversion");
+            double n=sc.nextDouble();
+
+
+            if(n>=1000000)
+            {
+                System.out.println("No. exceed maximum value..terminating");
+                return;
+            }
+
+            if(n<0)
+            {
+                System.out.println("No. is less than minimum value..terminating");
+                return;
+            }
+        
+            int intN=(int)n;
+            int n1=(int)(n*100);
+            String extra="";
+            int f = (n1-intN*100);
+            if(f > 0){
+                 extra+=String.valueOf(f)+"/100 ";
+            }
+
 		  //Print Word representation of given number n
-		  System.out.printf("Rs."+convertToWords(n)+"ONLY"); 
+		   System.out.printf("Rs."+convertToWords(intN)+extra+"ONLY"); 
+           return;
 	  }
 }
